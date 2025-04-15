f157a7ccd5ab35fc26f5ad12e93a121daea7bae5
Derek Zhu <derekzhu2005@gmail.com>
2025-04-15 16:19:00 -0700

    fixed freinds stuff

--- js/friends.js
+++ js/friends.js
@@ -1,41 +1,7 @@
-// import data from '../media/friends.json' with {type: "json"};
+import data from '../media/friends.json' with {type: "json"};
 
-// console.log(data);
+console.log(data);
 
-let data = {
-    "friends": [
-        {
-            "name": "Alex Niu",
-            "link": "https://www.linkedin.com/in/alexniu100/",
-            "desc": "A highly motivated software engineer with strong leadership and management skills.",
-            "skills": ["Fullstack Development", "Java", "Typescript"]
-        },
-        {
-            "name": "Hoang Ngyuen",
-            "link":"https://www.kipiiler.me/",
-            "desc":"Vietnamese",
-            "skills": ["C/C++", "Computer Graphics", "Ligma"]
-        },
-        {
-            "name": "Joshua Jung",
-            "link":"https://www.linkedin.com/in/joshua-jung04/",
-            "desc":"Extremely strong software developer with knack for building scalable systems.",
-            "skills": ["Fullstack Development", "Java", "Typescript"]
-        },
-        {
-            "name": "Micheal Li",
-            "link":"https://ml72.github.io/",
-            "desc":"Strong and passionate software developer & AI researcher",
-            "skills": ["Machine Learning", "AI Research", "Java"]
-        },
-        {
-            "name": "Eric Bae",
-            "link":"https://www.linkedin.com/in/ericbae1/",
-            "desc":"Visionary 🔭",
-            "skills": ["C/C++", "Computer Graphics", "Java"]
-        }
-    ]
-};
 
 data.friends.forEach((friend, i) => {
     console.log(friend);
